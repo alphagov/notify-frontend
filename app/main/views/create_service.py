4c063c6d7d7848c920f1cb37797b58dc0da41094
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-13 15:16:21 +0100

    Appeased pep8

--- app/main/views/create_service.py
+++ app/main/views/create_service.py
@@ -6,4 +6,3 @@ from . import get_template_data
 @main.route('/create-service', methods=['GET'])
 def view_create_service():
     return render_template("create-service.html", **get_template_data())
-
--- app/main/views/threefa.py
+++ app/main/views/threefa.py
@@ -6,4 +6,3 @@ from . import get_template_data
 @main.route('/3fa', methods=['GET'])
 def view_3fa():
     return render_template("3fa.html", **get_template_data())
-
