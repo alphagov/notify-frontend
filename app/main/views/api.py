fea3cb51e222293bb0663c289023ca01c7059858
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-19 18:28:53 +0100

    Added some wrappers to allow role based access to features / urls

--- app/main/views/api.py
+++ app/main/views/api.py
@@ -2,9 +2,10 @@ from flask import render_template, redirect
 from .. import main
 from . import get_template_data
 from flask_login import login_required, current_user
-
+from app.main.auth import role_required
 
 @main.route('/api', methods=['GET'])
 @login_required
+@role_required('admin')
 def view_api():
     return render_template("api.html", **get_template_data())
