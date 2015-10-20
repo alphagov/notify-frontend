d934a493d1bd2ea1a9c7a1468da31eda3a57046e
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-20 15:20:42 +0100

    Wired in new user-> service relationships
    - this replaces ORG as root object
    - all pages now based on users
    - services fetched by user and service id
    
    Also fixed all breadcrumbs and links

--- app/main/views/api.py
+++ app/main/views/api.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 from app.main.auth import role_required
 
 
-@main.route('/services/<int:service_id>/api', methods=['GET'])
+@main.route('/service/<int:service_id>/api', methods=['GET'])
 @login_required
 def view_api(service_id):
     return render_template("api.html", service_id=service_id, **get_template_data())
--- app/main/views/create_service.py
+++ app/main/views/create_service.py
@@ -4,6 +4,7 @@ from . import get_template_data
 from flask_login import login_required, current_user
 from app.main.forms import ServiceForm
 from app import data_api_client
+from app.api_client.errors import APIError
 
 
 @main.route('/create-service', methods=['GET'])
@@ -18,7 +19,10 @@ def view_create_service():
 def create_service():
     form = ServiceForm()
     if form.validate_on_submit():
-        data_api_client.create_service(form.service_name.data, int(session['organisation_id']))
-        return redirect(url_for('.view_all_services'))
+        try:
+            data_api_client.create_service(form.service_name.data, int(session['organisation_id']), int(session['user_id']))
+        except APIError as ex:
+            print(ex.message)
+        return redirect(url_for('.view_dashboard'))
     else:
         return render_template("create_service.html", form=form, **get_template_data())
